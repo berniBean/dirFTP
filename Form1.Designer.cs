35fe931a56af3c7ca7c4c68e436111f6cc51fb3e
berniBean <ernesto_fope55@hotmail.com>
2020-08-06 11:03:36 -0500

    cambiando dominio

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Servidor = new System.Windows.Forms.Label();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.rtbSalida = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Servidor
+            // 
+            this.Servidor.AutoSize = true;
+            this.Servidor.Location = new System.Drawing.Point(15, 48);
+            this.Servidor.Name = "Servidor";
+            this.Servidor.Size = new System.Drawing.Size(46, 13);
+            this.Servidor.TabIndex = 0;
+            this.Servidor.Text = "Servidor";
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(198, 48);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 1;
+            this.Usuario.Text = "Usuario";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(391, 48);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(52, 13);
+            this.password.TabIndex = 3;
+            this.password.Text = "password";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(67, 45);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(239, 45);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(443, 45);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 6;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(567, 43);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 7;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // rtbSalida
+            // 
+            this.rtbSalida.Location = new System.Drawing.Point(67, 101);
+            this.rtbSalida.Name = "rtbSalida";
+            this.rtbSalida.Size = new System.Drawing.Size(476, 322);
+            this.rtbSalida.TabIndex = 8;
+            this.rtbSalida.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(679, 450);
+            this.Controls.Add(this.rtbSalida);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.Servidor);
+            this.Name = "Form1";
+            this.Text = "Cliente FTP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Servidor;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.RichTextBox rtbSalida;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net;
+using System.IO;
+
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
@@ -15,6 +18,36 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("ftp://" + txtServidor.Text + "//");
+            FtpWebRequest ClienteRequest = (FtpWebRequest)WebRequest.Create(uri);
+
+            NetworkCredential credenciales = new NetworkCredential();
+
+            credenciales.Domain = "efloresp";
+            credenciales.UserName = txtUsuario.Text;
+            credenciales.Password = txtPass.Text;
+
+            ClienteRequest.Credentials = credenciales;
+            ClienteRequest.EnableSsl = false;
+            ClienteRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            ClienteRequest.KeepAlive = true;
+            ClienteRequest.UsePassive = true;
+
+            FtpWebResponse respuesta = (FtpWebResponse)ClienteRequest.GetResponse();
+
+            StreamReader sr = new StreamReader(respuesta.GetResponseStream(), Encoding.UTF8);
+            string resultado = sr.ReadToEnd();
+            rtbSalida.Text = resultado;
+            respuesta.Close();
+
+
+
+
+
         }
     }
 }
